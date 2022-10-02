a9c45f3c6fde19cdb6373091f16d1595cd2195c9
IlshatKhairullin <96662830+IlshatKhairullin@users.noreply.github.com>
2022-10-02 21:52:09 +0300

    + posts and posts detail view

--- web/views.py
+++ web/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .models import *
 
-# Create your views here.
+# class PostListView(ListView):
+#     queryset = Post.objects.all()
+#     context_object_name = 'posts'
+#     paginate_by = 4
+#     template_name = 'web/main_page.html'
+
+def post_list(request):
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 4)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'web/main_page.html',
+                  {'page': page,
+                   'posts': posts})
+
+
+def author(request):
+    pass
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request, 'web/detail.html', {'post': post})
