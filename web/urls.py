9a2da7fefd13b6b247db4e3da4d95b25616a0c90
IlshatKhairullin <96662830+IlshatKhairullin@users.noreply.github.com>
2022-10-06 18:05:51 +0300

    + login and register urls; changed home url

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, re_path
-
-
+from django.urls import path, re_path, include
 from web import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/' \
-        r'(?P<post>[-\w]+)/$', views.post_detail, name='post_detail')
+    path('', include('django.contrib.auth.urls')),
+    path('home/', views.PostListView.as_view(), name='post_list'),
+    path('register/', views.Register.as_view(), name='register'),
+    path('login/', views.login, name='login'),
+    re_path(r'^(?P<post>[-\w]+)/(?P<id>\d+)$', views.DetailPostView.as_view(),
+            name='post_detail')
 ]
