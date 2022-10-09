7c0144c4dddefff447cbbe851a1c66814858731c
IlshatKhairullin <96662830+IlshatKhairullin@users.noreply.github.com>
2022-10-09 22:08:12 +0300

    + class based views: PostEditDetailView, PostUpdateView, PostDeleteView; + draft view: new post add view

--- web/urls.py
+++ web/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('register/', views.Register.as_view(), name='register'),
     path('login/', views.login, name='login'),
     path('home/<str:title>/<int:id>', views.DetailPostView.as_view(), name='post_detail'),
-    path('profile/', views.ProfileView.as_view(), name='profile')
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('profile/<str:title>/<int:id>', views.DetailPostEditView.as_view(), name='post_edit_detail'),
+    path('profile/post-add/', views.post_edit_view, name='post_create'),
+    path('profile/<str:title>/<int:id>/delete', views.PostDeleteView.as_view(), name='post_delete'),
+    path('profile/<str:title>/<int:id>/edit', views.PostUpdateView.as_view(), name='post_edit')
 ]
