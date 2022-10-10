fab1d491dd0fa441c54562f462c8387e5316428c
Ильшат <ilshat1941@yandex.ru>
2022-10-10 10:30:43 +0300

    style: changed name of view

--- web/urls.py
+++ web/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('home/<str:title>/<int:id>', views.DetailPostView.as_view(), name='post_detail'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('profile/<str:title>/<int:id>', views.DetailPostEditView.as_view(), name='post_edit_detail'),
-    path('profile/post-add/', views.post_edit_view, name='post_create'),
+    path('profile/post-add/', views.post_add_view, name='post_create'),
     path('profile/<str:title>/<int:id>/delete', views.PostDeleteView.as_view(), name='post_delete'),
     path('profile/<str:title>/<int:id>/edit', views.PostUpdateView.as_view(), name='post_edit')
 ]
