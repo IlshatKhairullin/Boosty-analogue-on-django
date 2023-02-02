06751e6da98c93caccf677163a31b63970fb49e8
ilshatkhairullin <ilshat1941@yandex.ru>
2023-02-02 18:46:16 +0300

    feat: pytest and tests draft

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+def test_status(api_client):
+    response = api_client.get(reverse("status"))
+    assert response.status_code == status.HTTP_200_OK
+
+
+# def test_posts(api_client):
+#     response = api_client.get(reverse("posts"))
+#     assert response.status_code == status.HTTP_200_OK
+#     assert len(response.json()) > 0
--- src/api/urls.py
+++ src/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from api.views import status_view, posts_view, post_view
 
 urlpatterns = [
-    path("", status_view),
-    path("posts/", posts_view),
-    path("posts/<int:id>/", post_view),
+    path("", status_view, name="status"),
+    path("posts/", posts_view, name="posts"),
+    path("posts/<int:id>/", post_view, name="post"),
 ]
