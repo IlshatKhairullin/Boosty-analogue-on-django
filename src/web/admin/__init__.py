3b7d7ed617cf7eb2e6de3cb72927e4d774d3f577
ilshatkhairullin <ilshat1941@yandex.ru>
2023-02-08 09:23:51 +0300

    feat: user admin

--- src/web/admin/__init__.py
+++ src/web/admin/__init__.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from web.models import Post, Comment, User
 from web.admin.post import PostAdmin
 from web.admin.comment import CommentAdmin
-from web.admin.user import UserAdmin
+from web.admin.user import MyUserAdmin
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment, CommentAdmin)
-admin.site.register(User, UserAdmin)
+admin.site.register(User, MyUserAdmin)
--- src/web/admin/user.py
+++ src/web/admin/user.py
@@ -1,5 +1,41 @@
-from django.contrib import admin
+from web.admin.post import AuthorEmailDomainFilter
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 
-class UserAdmin(admin.ModelAdmin):
-    pass
+class EmailDomainFilter(AuthorEmailDomainFilter):
+    title = "Домен пользователя"
+    parameter_name = "user_email_domain"
+
+    # переопределили родительские атрибуты и метод queryset
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(email__endswith=f"@{self.value()}")
+        return queryset
+
+
+class MyUserAdmin(UserAdmin):
+    list_display = ("id", "email", "username", "is_staff")
+    ordering = ("-date_joined",)
+    list_filter = ("send_comment_on_post_notification", "date_joined", "last_login", "is_staff", EmailDomainFilter)
+    # поля внутри редактирования
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": ("is_staff",),
+            },
+        ),
+        (_("Dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
