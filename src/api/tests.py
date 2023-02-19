3ee8e5a53f5270774a7bce6fb83465538c82ee99
ilshatkhairullin <ilshat1941@yandex.ru>
2023-02-19 21:40:50 +0300

    feat: add api post pytest's

--- src/api/tests.py
+++ src/api/tests.py
@@ -3,18 +3,44 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from web.enums import Status
+
 
 @pytest.fixture
 def api_client():
     return APIClient()
 
 
-def test_status(api_client):
+def test_status(api_client):  # pytest's below
     response = api_client.get(reverse("status"))
     assert response.status_code == status.HTTP_200_OK
 
 
-# def test_posts(api_client):
-#     response = api_client.get(reverse("posts"))
-#     assert response.status_code == status.HTTP_200_OK
-#     assert len(response.json()) > 0
+def test_posts(api_client, post):  # post - обращение к фикстуре в conftest.py
+    response = api_client.get(reverse("posts"))
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()) > 0
+
+
+def test_post(api_client, post):
+    response = api_client.get(reverse("post", args=(post.id,)))
+    assert response.status_code == status.HTTP_200_OK
+    assert post.id == response.json()["id"]  # проверка на соответствие id созданного и того, что пришло
+
+
+def test_post_create(api_client, user):
+    response = api_client.post(
+        reverse("posts"),
+        data={"author": user, "title": "post title", "body": "lorem ipsum", "status": Status.published},
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+def test_post_full_update(api_client, post, user):
+    response = api_client.put(
+        reverse("post", args=(post.id,)),
+        data={"author": user, "title": "post title", "body": "lorem ipsum", "status": Status.published},
+    )
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    post.refresh_from_db()  # подтягиваем изменения из бд
+    assert post.title == "post title"
