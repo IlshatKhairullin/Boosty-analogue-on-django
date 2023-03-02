3459bd5f81fa9c0d211ddf53d1c259aa08a16777
ilshatkhairullin <ilshat1941@yandex.ru>
2023-03-02 21:01:36 +0300

    feat: add default router

--- src/api/urls.py
+++ src/api/urls.py
@@ -9,7 +9,11 @@ from api.views import status_view, posts_view, post_view, UserViewSet, NoteViewS
 # user/4/ - конкретный юзер
 router = routers.SimpleRouter()
 router.register(r"users", UserViewSet, basename="users")
-router.register(r"notes", NoteViewSet, basename="notes")
+
+# также вместо SimpleRouter можно объявить DefaultRouter, отличие только в том, что при запросе на главный урл
+# (сейчас он определен как status view), будет возвращаться ссылка на урл со списком элементов (post-list)
+default_router = routers.DefaultRouter()
+default_router.register(r"notes", NoteViewSet, basename="notes")
 
 # если же роутеры не подходят, то url с помощью viewset'ов можно написать так:
 # path('api/v1/userlist/', UserViewSet.as_view({'get': 'list'})), название в документации - viewset actions
@@ -27,11 +31,15 @@ schema_view = get_schema_view(
     permission_classes=[],
 )
 
-urlpatterns = [
-    path("", status_view, name="status"),
-    path("posts/", posts_view, name="posts"),
-    path("posts/<int:pk>/", post_view, name="post"),
-    re_path("^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-] + router.urls
+urlpatterns = (
+    [
+        path("", status_view, name="status"),
+        path("posts/", posts_view, name="posts"),
+        path("posts/<int:pk>/", post_view, name="post"),
+        re_path("^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+        path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+        path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    ]
+    + router.urls
+    + default_router.urls
+)
